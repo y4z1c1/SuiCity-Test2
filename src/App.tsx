e2e70c161b07ee8890d5a545d7106a94494ba003
nft entreprenaur <56488393+yay61@users.noreply.github.com>
2024-09-04 20:43:08 +0300

    made compatible with safari

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import PlayButton from './components/PlayButton';
 import Footer from './components/Footer';
+import WebFont from 'webfontloader';
+
 
 const App: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,6 +12,28 @@ const App: React.FC = () => {
   const handleModalState = (isOpen: boolean) => {
     setIsModalOpen(isOpen);
   };
+
+  useEffect(() => {
+    // Detect if the browser is Safari
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+    // If Safari is detected, add a class to the body
+    if (isSafari) {
+      document.body.classList.add('safari');
+    }
+  }, []); // Empty dependency array ensures this runs once on mount
+
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Fashion Norsa', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      active() {
+        console.log('Fonts loaded');
+      },
+    });
+  }, []);
 
   return (
     <div>
