f55a646c719e774e3a79ed7ea98e7f82127053b5
nft entreprenaur <56488393+yay61@users.noreply.github.com>
2024-09-06 21:50:20 +0300

    removed fontloader

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,16 @@ import Header from "./components/Header";
 import PlayButton from "./components/PlayButton";
 import Footer from "./components/Footer";
 import Section from "./components/Section"; // Use the correct file casing
-import WebFont from "webfontloader";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Guide from "./components/Guide";
 import Faq from "./components/Faq"; // Import the Faq component
 
 const App: React.FC = () => {
-  useEffect(() => {
-    WebFont.load({
-      custom: {
-        families: [
-          "Fashion Norsa",
-          "Helvetica",
-          "Helvetica Oblique",
-          "Helvetica Compressed",
-          "Helvetica Rounded Bold",
-          "Helvetica Bold",
-          "Helvetica Bold Oblique",
-          "Helvetica Light",
-        ],
-        urls: ["./assets/styles/fonts.css"], // Update this to the actual path of your CSS file that includes font-face
-      },
-      active() {
-        console.log("Fonts loaded");
-      },
-    });
-  }, []);
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleModalState = (isOpen: boolean) => {
     setIsModalOpen(isOpen);
   };
-
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ["Fashion Norsa", "Helvetica", "Arial", "sans-serif"],
-      },
-      active() {
-        console.log("Fonts loaded");
-      },
-    });
-  }, []);
 
   return (
     <Router>
