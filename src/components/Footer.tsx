0154ca4c77613fbce64e00a9259fac892ff45cc2
nft entreprenaur <56488393+yay61@users.noreply.github.com>
2024-09-07 18:42:48 +0300

    head update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,34 @@
 import React from "react";
 import "../assets/styles/Footer.css";
+import { FaTwitter, FaDiscord, FaEnvelope } from "react-icons/fa"; // Import icons from react-icons
 
 const Footer: React.FC = () => {
   return (
-    // Inside Footer.tsx
     <footer className="footer" id="footer">
       <div>
         <ul className="footer-links">
           <li>
-            <a href="https://twitter.com/suicityp2e">Twitter</a>
+            <a
+              href="https://twitter.com/suicityp2e"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="footer-icon" />
+            </a>
           </li>
           <li>
-            <a>Discord</a>
+            <a
+              href="#"
+              className="discord-soon"
+              title="Coming Soon" // Tooltip message
+            >
+              <FaDiscord className="footer-icon" />
+            </a>
           </li>
           <li>
-            <a href="mailto:suicity.p2e@gmail.com">Mail</a>
+            <a href="mailto:suicity.p2e@gmail.com">
+              <FaEnvelope className="footer-icon" />
+            </a>
           </li>
         </ul>
       </div>
