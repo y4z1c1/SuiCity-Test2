d5af4e99eca4e543db7d1c7f931b9ac178429bbc
nft entreprenaur <56488393+yay61@users.noreply.github.com>
2024-09-14 14:23:48 +0300

    play demo

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,42 +1,16 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
+import React from "react";
 import "../assets/styles/PlayButton.css";
 
-interface PlayButtonProps {
-  onModalStateChange: (isOpen: boolean) => void;
-}
-
-const PlayButton: React.FC<PlayButtonProps> = ({ onModalStateChange }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const openModal = () => {
-    setModalIsOpen(true);
-    onModalStateChange(true); // Notify parent to apply blur
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-    onModalStateChange(false); // Notify parent to remove blur
+const PlayButton: React.FC = () => {
+  const redirectToDemo = () => {
+    window.location.href = "https://demo.suicityp2e.com";
   };
 
   return (
-    <div>
-      <div className="play-button-container">
-        <button className="play-button" onClick={openModal}>
-          Play
-        </button>
-      </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        className="modal"
-        overlayClassName="overlay"
-      >
-        <h2>Whoa there, eager beaver! The game's not quite cooked yet!</h2>
-        <button onClick={closeModal} className="close-modal-button">
-          Close
-        </button>
-      </Modal>
+    <div className="play-button-container">
+      <button className="play-button" onClick={redirectToDemo}>
+        Play Demo
+      </button>
     </div>
   );
 };
